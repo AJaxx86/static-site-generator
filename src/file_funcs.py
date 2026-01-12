8e9e341b509f1108e37c3a83e179d11d7d932275
AJaxx86 <62261949+AJaxx86@users.noreply.github.com>
2026-01-12 19:22:20 +0000

    still in progress, up to step 5.4.

--- src/file_funcs.py
+++ src/file_funcs.py
@@ -1,12 +1,20 @@
 import os
 import shutil
 
+
 def static_to_public():
-    if not os.path.exists('static'):
-        raise Exception('Static directory does not exist')
+    if not os.path.exists("static"):
+        raise Exception("Static directory does not exist")
 
-    if os.path.exists('public'):
-        print('Public already exists. Removing...')
-        shutil.rmtree('public')
+    if os.path.exists("public"):
+        print("Public already exists. Removing...")
+        shutil.rmtree("public")
 
-    shutil.copytree('static', 'public')
+    shutil.copytree("static", "public")
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
+
+    md_content = open(from_path, "r").read()
+    template = open(template_path, "r").read()
--- src/md_format.py
+++ src/md_format.py
@@ -1,7 +1,8 @@
 import re
 from enum import Enum
-from textnode import TextNode, TextType
+
 from htmlnode import HTMLNode, LeafNode, ParentNode, text_node_to_html_node
+from textnode import TextNode, TextType
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -10,6 +11,7 @@ class BlockType(Enum):
     QUOTE = "quote"
     UNORDERED_LIST = "unordered_list"
     ORDERED_LIST = "ordered_list"
+
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
@@ -25,7 +27,9 @@ def markdown_to_html_node(markdown):
         if block_type == BlockType.HEADING:
             h_count = len(block) - len(block.lstrip("#"))
             filtered_text = block.lstrip("# ")
-            html_parent.children.append(ParentNode(f"h{h_count}", text_to_child_nodes(filtered_text)))
+            html_parent.children.append(
+                ParentNode(f"h{h_count}", text_to_child_nodes(filtered_text))
+            )
 
         if block_type == BlockType.CODE:
             filtered_block = block.lstrip("```\n").rstrip("```")
@@ -34,8 +38,10 @@ def markdown_to_html_node(markdown):
 
         if block_type == BlockType.QUOTE:
             filtered_block = block.lstrip("> ").rstrip("\n")
-            html_parent.children.append(ParentNode("blockquote", text_to_child_nodes(filtered_block)))
-        
+            html_parent.children.append(
+                ParentNode("blockquote", text_to_child_nodes(filtered_block))
+            )
+
         if block_type == BlockType.UNORDERED_LIST:
             list_items = block.split("\n")
             child_nodes = []
@@ -44,22 +50,24 @@ def markdown_to_html_node(markdown):
                 child_nodes.append(ParentNode("li", text_to_child_nodes(text)))
 
             html_parent.children.append(ParentNode("ul", child_nodes))
-        
+
         if block_type == BlockType.ORDERED_LIST:
             list_items = block.split("\n")
             child_nodes = []
             for i in range(len(list_items)):
                 text = list_items[i].split(". ", 1)[1]
                 child_nodes.append(ParentNode("li", text_to_child_nodes(text)))
-                
+
             html_parent.children.append(ParentNode("ol", child_nodes))
-    
+
     return html_parent
+
 
 def text_to_child_nodes(text):
     text_nodes = text_to_textnodes(text)
     html_nodes = [text_node_to_html_node(node) for node in text_nodes]
     return html_nodes
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -70,7 +78,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
         split_text = node.text.split(delimiter)
         if len(split_text) % 2 == 0:
-            raise Exception(f"Text doesn't contain enough delimiters.\nNode: {node}\nSplit text: {split_text}")
+            raise Exception(
+                f"Text doesn't contain enough delimiters.\nNode: {node}\nSplit text: {split_text}"
+            )
 
         for i in range(len(split_text)):
             if split_text[i] == "":
@@ -82,9 +92,10 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
     return new_nodes
 
+
 def split_nodes_image(old_nodes):
     new_nodes = []
-    
+
     for node in old_nodes:
         original_text = node.text
         sections = extract_markdown_images(original_text)
@@ -99,7 +110,7 @@ def split_nodes_image(old_nodes):
             new_nodes.append(TextNode(split_text[0], TextType.TEXT))
             new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
             original_text = split_text[1]
-        
+
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
 
@@ -110,9 +121,10 @@ def split_nodes_image(old_nodes):
             new_nodes.remove(node)
     return new_nodes
 
+
 def split_nodes_link(old_nodes):
     new_nodes = []
-    
+
     for node in old_nodes:
         original_text = node.text
         sections = extract_markdown_links(original_text)
@@ -127,7 +139,7 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(split_text[0], TextType.TEXT))
             new_nodes.append(TextNode(link_text, TextType.LINK, link))
             original_text = split_text[1]
-        
+
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
 
@@ -136,13 +148,15 @@ def split_nodes_link(old_nodes):
             new_nodes.remove(node)
     return new_nodes
 
+
 def text_to_textnodes(text):
     new_nodes = split_nodes_image(split_nodes_link([TextNode(text, TextType.TEXT)]))
     new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
     new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
     new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
-    
+
     return new_nodes
+
 
 def markdown_to_blocks(text):
     md_blocks = text.split("\n\n")
@@ -153,6 +167,7 @@ def markdown_to_blocks(text):
             continue
         filtered_blocks.append(block)
     return filtered_blocks
+
 
 def block_to_block_type(md_block):
     if md_block.startswith("#"):
@@ -167,14 +182,19 @@ def block_to_block_type(md_block):
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
 
+
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
+
 def extract_title(markdown: str) -> str:
     title_match = re.search(r"^# (.*)$", markdown, re.MULTILINE)
     if title_match:
+        return title_match.group(1)
+    raise Exception(f"Title could not be found in markdown: {markdown}")
         return title_match.group(1)
     raise Exception(f'Title could not be found in markdown: {markdown}')
